9196fe1c201c9197b4e4f746539c9ea49f45b9f6
Egor <eriskovets@gmail.com>
2021-12-15 20:53:33 +0500

    add popup component

--- app/components/Popup/Popup.tsx
+++ app/components/Popup/Popup.tsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { FunctionComponent, ReactNode } from 'react'
+
+import './Popup.scss'
 
 export type PropTypes = {
-  overlay: boolean;
-  className: string;
-  close(): void;
-  children: React.ReactNode;
+  overlay: boolean
+  className: string
+  close?(): void
+  children?: ReactNode
 }
 
-const Popup: React.FunctionComponent<PropTypes> = () => {
-  return (
-    <div>
 
+const Popup: FunctionComponent<PropTypes> = ({
+  overlay,
+  className,
+  close,
+  children
+}) => {
+  return (
+    <div className={`${className} Popup-decoration`}>
+      <div className={'Popup__content Popup__content-decoration'}>
+        <div className="Popup__contentHeader">
+          <button className="Popup__closeBtn" onClick={close}>X</button>
+        </div>
+        <div>
+          {children}
+        </div>
+      </div>
+      <div className="overlay"></div>
     </div>
   )
 }
--- app/pages/Home/Home.tsx
+++ app/pages/Home/Home.tsx
@@ -4,12 +4,19 @@ interface HomeProps {} // eslint-disable-line @typescript-eslint/no-empty-interf
 interface HomeState {} // eslint-disable-line @typescript-eslint/no-empty-interface
 
 import './Home.scss'
+import Popup from '@Components/Popup'
 
 class Home extends React.Component<HomeProps, HomeState> {
   render() {
     return(
       <div>
         Home page
+        <Popup
+          close={()=> console.log('close')}
+          overlay={true}
+          className={'Popup'}>
+          <div>Lorem ipsum dolor sit amet, consectetur.</div>
+        </Popup>
       </div>
     )
   }
