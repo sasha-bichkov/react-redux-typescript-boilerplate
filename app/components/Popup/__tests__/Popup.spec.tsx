70aa4e0259645317b7b09f7fced54d68c72580f4
Egor <eriskovets@gmail.com>
2021-12-16 23:22:44 +0500

    fix popup scss

--- app/components/Popup/__tests__/Popup.spec.tsx
+++ app/components/Popup/__tests__/Popup.spec.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Popup from '../Popup'
 
-describe('should render Popup component', () => {
-  it('should contain .Popup className', () => {
+describe('Popup', () => {
+  it('should render Popup component', () => {
     const component = shallow(
-      <Popup overlay={true} close={()=> console.log('click')}/>
+      <Popup overlay={true} close={() => console.log('click')} />
     )
     expect(component).toMatchSnapshot()
   })
