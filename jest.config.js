aac65656582216882b99ee5117a76554d63306ce
Sasha Bychkov <aleksandar.bichkov@gmail.com>
2021-12-18 10:40:23 +0500

    Stubbed assets for jest specs

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,11 @@ module.exports = {
   ],
   moduleNameMapper: {
     '^@Root/(.*)$': '<rootDir>/app/$1',
+    '^@Pages/(.*)$': '<rootDir>/app/pages/$1',
+    '^@Modules/(.*)$': '<rootDir>/app/modules/$1',
+    // '^@Images/(.*)$': '<rootDir>/public/images/$1',
+    '^@Components/(.*)$': '<rootDir>/app/components/$1',
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/app/fileMock.ts',
+    '\\.(s?css|less)$': '<rootDir>/app/fileMock.ts'
   }
 }
