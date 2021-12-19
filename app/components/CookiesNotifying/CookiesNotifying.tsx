4066bc8909cf9d26e8126718d00795af0abd2292
Egor <eriskovets@gmail.com>
2021-12-19 20:18:48 +0500

    add CookiesNotifying component

--- app/components/CookiesNotifying/CookiesNotifying.tsx
+++ app/components/CookiesNotifying/CookiesNotifying.tsx
@@ -8,18 +8,21 @@ import './CookiesNotifying.scss'
 const CookiesNotifying = () => {
   return (
     <div className="CookiesNotifying">
-      <div className="CookiesNotifying__leftColumn">
-        <Warning className="CookiesNotifying__icon" width="25" />
-        <p className="CookiesNotifying__text">
-          We use cookies to personalise content and ads,
-          to provide social media features and to analyse our traffic.
-          We also share information about your use of our site with our social media,
-          advertising and analytics partners who may combine it with other information
-          that you’ve provided to them or that they’ve collected from your use of their services.
-        </p>
-      </div>
-      <div className="CookiesNotifying__rightColumn">
-        <Button className="CookiesNotifying__btn" caption="accept and close" />
+      <div className="CookiesNotifying__content">
+        <div className="CookiesNotifying__leftColumn">
+          <Warning className="CookiesNotifying__icon" width="25" />
+          <p className="CookiesNotifying__text">
+            We&nbsp;use cookies to&nbsp;personalise content and ads,
+            to&nbsp;provide social media features and to&nbsp;analyse our traffic.
+            We&nbsp;also share information about your use of&nbsp;our site with our social media,
+            advertising and analytics partners who may combine it&nbsp;with other information
+            that you&rsquo;ve provided to&nbsp;them or&nbsp;that
+            they&rsquo;ve collected from your use of&nbsp;their services.
+          </p>
+        </div>
+        <div className="CookiesNotifying__rightColumn">
+          <Button className="CookiesNotifying__btn" caption="accept and close" />
+        </div>
       </div>
     </div>
   )
