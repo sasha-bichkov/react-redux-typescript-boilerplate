733a2b78a9f381b5a170b9d9b9deaf9e83b83842
Egor <eriskovets@gmail.com>
2021-12-27 19:58:28 +0500

    add tests

--- app/components/Popup/__tests__/Popup.spec.tsx
+++ app/components/Popup/__tests__/Popup.spec.tsx
@@ -1,12 +1,49 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import Popup from '../Popup'
 
 describe('Popup', () => {
-  it('should render Popup component', () => {
-    const component = shallow(
-      <Popup overlay={true} close={() => console.log('click')} />
+  it('calls button onclick callback at once if click button', () => {
+    const mockCallback = jest.fn()
+    const component = mount(
+      <Popup
+        overlay={true}
+        close={() => console.log('click')}
+      />
     )
-    expect(component).toMatchSnapshot()
+
+    component.find('.Popup__closeButton').simulate('click')
+
+    expect(mockCallback).toHaveBeenCalledTimes(0)
+  })
+
+  describe('if overlay is true', () => {
+    it('renders background', () => {
+      const component = mount(
+        <Popup
+          overlay={true}
+          close={() => console.log('click')}
+        />
+      )
+
+      const result = component.containsMatchingElement(<div className="overlay" />)
+
+      expect(result).toBeTruthy()
+    })
+  })
+
+  describe('if overlay is false', () => {
+    it('does not render background', () => {
+      const component = mount(
+        <Popup
+          overlay={true}
+          close={() => console.log('click')}
+        />
+      )
+
+      const result = component.containsMatchingElement(<div className="overlay" />)
+
+      expect(result).toBeFalsy()
+    })
   })
 })
