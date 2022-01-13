bd9a6810da4e8b4c39597391da02aab4f6aef359
Sasha Bychkov <aleksandar.bichkov@gmail.com>
2022-01-13 10:56:00 +0500

    Fixed specs with mockedAxios

--- app/utils/isEmailDisposable/__tests__/isEmailDisposable.spec.tsx
+++ app/utils/isEmailDisposable/__tests__/isEmailDisposable.spec.tsx
@@ -5,32 +5,36 @@ import axios from 'axios'
 import isEmailDisposable, { DISPOSABLE_EMAIL_CHECKER_URL } from '../isEmailDisposable'
 
 jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
 
 describe('isEmailDisposable', () => {
   const URL = DISPOSABLE_EMAIL_CHECKER_URL
 
   describe('if url path correct', () => {
     it('sends a request to the correct url', async () => {
-      const resp = {
+      const response = {
         data: {
           disposable: false
         }
       }
-      axios.get.mockResolvedValueOnce(resp)
+
+      mockedAxios.get.mockResolvedValueOnce(response)
+
       await isEmailDisposable('test@test.com')
 
-      expect(axios.get).toHaveBeenCalledWith(`${URL}test@test.com`)
+      expect(mockedAxios.get).toHaveBeenCalledWith(`${URL}test@test.com`)
     })
   })
 
   describe('if it is real email', () => {
     it('returns false', async () => {
-      const resp = {
+      const response = {
         data: {
           disposable: false
         }
       }
-      axios.get.mockResolvedValueOnce(resp)
+
+      mockedAxios.get.mockResolvedValueOnce(response)
       const result = await isEmailDisposable('test@test.com')
 
       expect(result).toBeFalsy()
@@ -39,12 +43,13 @@ describe('isEmailDisposable', () => {
 
   describe('if it is disposable email', () => {
     it('returns true', async () => {
-      const resp = {
+      const response = {
         data: {
           disposable: true
         }
       }
-      axios.get.mockResolvedValueOnce(resp)
+
+      mockedAxios.get.mockResolvedValueOnce(response)
       const result = await isEmailDisposable('konamey234@wusehe.com')
 
       expect(result).toBeTruthy()
--- app/utils/isEmailDisposable/isEmailDisposable.ts
+++ app/utils/isEmailDisposable/isEmailDisposable.ts
@@ -3,7 +3,6 @@ import axios from 'axios'
 export const DISPOSABLE_EMAIL_CHECKER_URL = 'https://open.kickbox.com/v1/disposable/'
 
 export default async function isEmailDisposable(email: string): Promise<boolean> {
-  const {data} = await axios.get(`${DISPOSABLE_EMAIL_CHECKER_URL}${email}`)
-  return data.disposable
+  const response = await axios.get(`${DISPOSABLE_EMAIL_CHECKER_URL}${email}`)
+  return response.data.disposable
 }
-
