41bfbde5a20162c3b2ed043fc9fbe3b29b673978
Egor <eriskovets@gmail.com>
2022-01-19 00:09:29 +0500

    add test UnsupportedBrowser

--- app/components/UnsupportedBrowser/__tests__/BrowserItem.spec.tsx
+++ app/components/UnsupportedBrowser/__tests__/BrowserItem.spec.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 import UnsupportedBrowser from '../UnsupportedBrowser'
 
 describe('UnsupportedBrowser', () => {
-  it('1', () => {
-    render(<UnsupportedBrowser />)
+  it('matches snapshot', () => {
+    const { asFragment } = render(<UnsupportedBrowser />)
+
+    expect(asFragment()).toMatchSnapshot()
   })
 })
