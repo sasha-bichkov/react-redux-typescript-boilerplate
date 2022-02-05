ace61c9f4d6ece2509202c6b87d5670d9f64adb9
Sasha Bychkov <aleksandar.bichkov@gmail.com>
2022-02-05 23:40:40 +0500

    Added descriptions for specs

--- app/utils/convertSnakeCase/__tests__/convertSnakeCase.spec.ts
+++ app/utils/convertSnakeCase/__tests__/convertSnakeCase.spec.ts
@@ -2,121 +2,137 @@ import { convertToSnakeCase } from '../convertSnakeCase'
 
 describe('converToSnakeCase', () => {
 
-  it('works [1]', () => {
-    const testObject = {}
+  describe('if an object without properties is passed', () => {
+    it('returns it', () => {
+      const testObject = {}
 
-    const result = convertToSnakeCase(testObject)
+      const result = convertToSnakeCase(testObject)
 
-    expect(result).toEqual({})
-  })
-
-  it('works [2]', () => {
-    const testObject = {
-      my_param: 'value'
-    }
-
-    const result = convertToSnakeCase(testObject)
-
-    expect(result).toEqual(testObject)
-  })
-
-  it('works [3]', () => {
-    const testObject = {
-      myParam: 'value'
-    }
-
-    const result = convertToSnakeCase(testObject)
-
-    expect(result).toEqual({
-      my_param: 'value'
+      expect(result).toEqual({})
     })
   })
 
-  it('works [4]', () => {
-    const testObject = {
-      myParam: 'value',
-      myParam2: 'value',
-      myParam3: 'value',
-      my_param4: 'value',
-    }
-
-    const result = convertToSnakeCase(testObject)
-
-    expect(result).toEqual({
-      my_param: 'value',
-      my_param_2: 'value',
-      my_param_3: 'value',
-      my_param_4: 'value',
-    })
-  })
-
-  it('works [5]', () => {
-    const testObject = {
-      myParam: {
-        myParam2: 'value'
+  describe('if an object with a snake case param is passed', () => {
+    it('returns the object as it is', () => {
+      const testObject = {
+        my_param: 'value'
       }
-    }
 
-    const result = convertToSnakeCase(testObject)
+      const result = convertToSnakeCase(testObject)
 
-    expect(result).toEqual({
-      my_param: {
-        my_param_2: 'value'
+      expect(result).toEqual(testObject)
+    })
+  })
+
+  describe('if an object with a camelCase param is passed', () => {
+    it('returns an object with snake case params', () => {
+      const testObject = {
+        myParam: 'value'
       }
+
+      const result = convertToSnakeCase(testObject)
+
+      expect(result).toEqual({
+        my_param: 'value'
+      })
     })
   })
 
-  it('works [6]', () => {
-    const testObject = {
-      myParam: {
-        myParam2: 'value'
-      },
-      myParam2: 'value2'
-    }
+  describe('if multiple params are passed', () => {
+    it('returns an object with snake case params', () => {
+      const testObject = {
+        myParam: 'value',
+        myParam2: 'value',
+        myParam3: 'value',
+        my_param4: 'value',
+      }
 
-    const result = convertToSnakeCase(testObject)
+      const result = convertToSnakeCase(testObject)
 
-    expect(result).toEqual({
-      my_param: {
-        my_param_2: 'value'
-      },
-      my_param_2: 'value2'
+      expect(result).toEqual({
+        my_param: 'value',
+        my_param_2: 'value',
+        my_param_3: 'value',
+        my_param_4: 'value',
+      })
     })
   })
 
-  it('works [7]', () => {
-    const testObject = {
-      myParam: {
-        myParam2: {
-          myParam3: 'value'
+  describe('if a nested param is passed', () => {
+    it('returns an object with snake case params', () => {
+      const testObject = {
+        myParam: {
+          myParam2: 'value'
         }
       }
-    }
 
-    const result = convertToSnakeCase(testObject)
+      const result = convertToSnakeCase(testObject)
 
-    expect(result).toEqual({
-      my_param: {
-        my_param_2: {
-          my_param_3: 'value'
+      expect(result).toEqual({
+        my_param: {
+          my_param_2: 'value'
         }
-      }
+      })
     })
   })
 
-  it('works [8]', () => {
-    const testObject = {
-      myParam: {
-        myParam2: {
-          myParam3: {
-            myParam4: {
-              myParam5: {
-                myParam6: {
-                  myParam7: {
-                    myParam8: {
-                      myParam9: {
-                        myParam10: {
-                          myParam11: 'test'
+  describe('if there are multiple params', () => {
+    it('returns an object with snake case params', () => {
+      const testObject = {
+        myParam: {
+          myParam2: 'value'
+        },
+        myParam2: 'value2'
+      }
+
+      const result = convertToSnakeCase(testObject)
+
+      expect(result).toEqual({
+        my_param: {
+          my_param_2: 'value'
+        },
+        my_param_2: 'value2'
+      })
+    })
+  })
+
+  describe('if there is nesting 3 levels in depth', () => {
+    it('returns an object with snake case params', () => {
+      const testObject = {
+        myParam: {
+          myParam2: {
+            myParam3: 'value'
+          }
+        }
+      }
+
+      const result = convertToSnakeCase(testObject)
+
+      expect(result).toEqual({
+        my_param: {
+          my_param_2: {
+            my_param_3: 'value'
+          }
+        }
+      })
+    })
+  })
+
+  describe('if there is nesting 11 levels in depth', () => {
+    it('returns an object with snake case params', () => {
+      const testObject = {
+        myParam: {
+          myParam2: {
+            myParam3: {
+              myParam4: {
+                myParam5: {
+                  myParam6: {
+                    myParam7: {
+                      myParam8: {
+                        myParam9: {
+                          myParam10: {
+                            myParam11: 'test'
+                          }
                         }
                       }
                     }
@@ -127,22 +143,22 @@ describe('converToSnakeCase', () => {
           }
         }
       }
-    }
 
-    const result = convertToSnakeCase(testObject)
+      const result = convertToSnakeCase(testObject)
 
-    expect(result).toEqual({
-      my_param: {
-        my_param_2: {
-          my_param_3: {
-            my_param_4: {
-              my_param_5: {
-                my_param_6: {
-                  my_param_7: {
-                    my_param_8: {
-                      my_param_9: {
-                        my_param_10: {
-                          my_param_11: 'test'
+      expect(result).toEqual({
+        my_param: {
+          my_param_2: {
+            my_param_3: {
+              my_param_4: {
+                my_param_5: {
+                  my_param_6: {
+                    my_param_7: {
+                      my_param_8: {
+                        my_param_9: {
+                          my_param_10: {
+                            my_param_11: 'test'
+                          }
                         }
                       }
                     }
@@ -152,29 +168,31 @@ describe('converToSnakeCase', () => {
             }
           }
         }
-      }
+      })
     })
   })
 
-  it('works [9]', () => {
-    const testObject = {
-      test: 'value',
-      myParam4: {
-        test: 'value'
-      },
-      my: 'value',
-      myArray: [1, 2, 3, 4, 5]
-    }
+  describe('if there is an array as a value', () => {
+    it('returns an object with snake case params', () => {
+      const testObject = {
+        test: 'value',
+        myParam4: {
+          test: 'value'
+        },
+        my: 'value',
+        myArray: [1, 2, 3, 4, 5]
+      }
 
-    const result = convertToSnakeCase(testObject)
+      const result = convertToSnakeCase(testObject)
 
-    expect(result).toEqual({
-      test: 'value',
-      my_param_4: {
-        test: 'value'
-      },
-      my: 'value',
-      my_array: [1, 2, 3, 4, 5]
+      expect(result).toEqual({
+        test: 'value',
+        my_param_4: {
+          test: 'value'
+        },
+        my: 'value',
+        my_array: [1, 2, 3, 4, 5]
+      })
     })
   })
 })
--- app/utils/convertSnakeCase/convertSnakeCase.ts
+++ app/utils/convertSnakeCase/convertSnakeCase.ts
@@ -1,5 +1,7 @@
 import { snakeCase, mapValues, mapKeys, isPlainObject } from 'lodash'
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 interface IObject {
   [key: string]: any
 }
@@ -8,12 +10,12 @@ export function convertToSnakeCase(hash: IObject) {
   return mapKeysDeep(hash, snakeCase)
 }
 
-const mapKeysDeep = (hash: IObject, callback: (key: any) => {}): any[any] => {
-  const keys = mapKeys(hash, (value, key) => {
+const mapKeysDeep = (hash: IObject, callback: (key: string) => object): object => {
+  const keys = mapKeys(hash, (value: any, key: string) => {
     return callback(key)
   })
 
-  return mapValues(keys, (val) => {
+  return mapValues(keys, (val: any) => {
     return isPlainObject(val) ? mapKeysDeep(val, callback) : val
   })
 }
